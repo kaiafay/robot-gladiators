944cdf29bc4be5ba56623dc26922bd40dd184db2
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-07 17:48:30 -0800

    randomize fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,41 +27,53 @@ var fightOrSkip = function() {
 }
 
 var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    };
+    console.log(isPlayerTurn);
     // Game begins
    while(playerInfo.health > 0 && enemy.health > 0) {
-        // Choice to fight or skip the battle
-          if (fightOrSkip()) {
-              break;
-          };
-        // Player attacks enemy
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-        enemy.health = Math.max(0, enemy.health - damage);
-        console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
-
-        // Enemy attacks player
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
-
-        // Check enemy's health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died!");
-
-            // award player money for winning
-            playerInfo.money = playerInfo.money + 20;
-            break; 
-        } else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-        }
-
-        // Check player's health
-        if (playerInfo.health <= 0) {
-            window.alert(playerInfo.name + " has died!");
+        if (isPlayerTurn) {
+            // Choice to fight or skip the battle
+            if (fightOrSkip()) {
             break;
-        } else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            };
+    
+            // Player attacks enemy
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
+
+            // Enemy attacks player
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
+
+            // Check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+
+                // award player money for winning
+                playerInfo.money = playerInfo.money + 20;
+                break; 
+            } else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+
+            // Check player's health
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            } else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
-    }
+
+        // switch turn order for next round
+        isPlayerTurn = !isPlayerTurn
+   }
 };
 
 var startGame = function() {
